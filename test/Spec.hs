adde19bedd61d41fb90d3b43c4dba95c90291151
Thomas Busby <tom@busby.ninja>
2018-08-04 19:03:36 +0200

    Add testing dependencies and ensure they work

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+    describe "Test" $ do
+        it "checks that the hspec deps are working" $
+            3 `shouldBe` (2 + 1)
+        it "checks that QuickCheck is working" $
+            property $ \x -> id x == (x :: Int)
