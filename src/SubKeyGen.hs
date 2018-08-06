21334ff8e37f71e82f898b7c7c274b1600d95acd
Tom Busby <tom@busby.ninja>
2018-08-07 00:23:41 +0200

    Implement SubKeyGen g function

--- src/SubKeyGen.hs
+++ src/SubKeyGen.hs
@@ -3,14 +3,16 @@ module SubKeyGen
     ) where
 
 import Data.Word
+import Data.Bits (xor)
 import Crypto.Number.F2m (modF2m)
 
 import Globals (Key, numRounds, aesPolynomial)
+import SBox(sBox)
 
 type KeyWord = [Word8]
-type RC = [Word8]
+type RC = Word8
 
-rcVals :: [Word8]
+rcVals :: [RC]
 rcVals = map (fromIntegral . modF2m aesPolynomial) (take numRounds $ doubles 1)
     where
         doubles n = n:(doubles $ 2 * n)
@@ -19,4 +21,7 @@ generateSubKeys :: Key -> [Key]
 generateSubKeys = undefined
 
 g :: RC -> KeyWord -> KeyWord
-g = undefined
+g rc kw = rcXor . map sBox $ kwRot
+    where
+        kwRot = let (l, r) = splitAt 1 kw in r ++ l
+        rcXor (x:xs) = (rc `xor` x):xs
