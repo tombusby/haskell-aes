c7f2cbcfc8427d83f290fd5c3272b25a14c3a04a
Tom Busby <tom@busby.ninja>
2018-08-07 01:11:32 +0200

    Implement subkey generation

--- src/SubKeyGen.hs
+++ src/SubKeyGen.hs
@@ -2,8 +2,10 @@ module SubKeyGen
     ( generateSubKeys
     ) where
 
+import Control.Monad.State.Lazy
 import Data.Word
 import Data.Bits (xor)
+import Data.List.Split (chunksOf)
 import Crypto.Number.F2m (modF2m)
 
 import Globals (Key, numRounds, aesPolynomial)
@@ -13,7 +15,34 @@ type KeyWord = [Word8]
 type RC = Word8
 
 generateSubKeys :: Key -> [Key]
-generateSubKeys = undefined
+generateSubKeys k = reverse keys
+    where
+        keys = snd . execState (keyGen numRounds k) $ (rcVals, [])
+
+pushSubkey :: Key -> State ([RC], [Key]) ()
+pushSubkey k = do
+    (rcs, sks) <- get
+    put (rcs, k:sks)
+
+popRC :: State ([RC], [Key]) RC
+popRC = do
+    (rc:rcs, sks) <- get
+    put (rcs, sks)
+    return rc
+
+keyGen :: Int -> Key -> State ([RC], [Key]) ()
+keyGen 0 _ = return ()
+keyGen n k = do
+    rc <- popRC
+    w0' <- return $ w0 `mapXor` g rc w3
+    w1' <- return $ w0' `mapXor` w1
+    w2' <- return $ w1' `mapXor` w2
+    w3' <- return $ w2' `mapXor` w3
+    pushSubkey . concat $ [w0', w1', w2', w3']
+    keyGen (n-1) . concat $ [w0', w1', w2', w3']
+        where
+            mapXor = zipWith xor
+            [w0, w1, w2, w3] = chunksOf 4 k
 
 rcVals :: [RC]
 rcVals = map (fromIntegral . modF2m aesPolynomial) (take numRounds $ doubles 1)
--- test/SubKeyGenSpec.hs
+++ test/SubKeyGenSpec.hs
@@ -6,8 +6,19 @@ import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
+import Globals (Key)
+import SubKeyGen
+
+key :: Key
+key = [0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6,
+    0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C]
+
+subKey1 :: Key
+subKey1 = [0xa0, 0xfa, 0xfe, 0x17, 0x88, 0x54, 0x2c, 0xb1,
+    0x23, 0xa3, 0x39, 0x39, 0x2a, 0x6c, 0x76, 0x05]
+
 runTests :: IO ()
 runTests = hspec $ do
     describe "generateSubKeys" $ do
         it "checks that, given a key, it prodeces the correct list of subkeys" $
-            pendingWith "need to implement keygen"
+            ((!!0) . generateSubKeys $ key) `shouldBe` subKey1
