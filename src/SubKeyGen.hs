d7b323f1e0b4fd8e04e199b62e21f242561a1eee
Tom Busby <tom@busby.ninja>
2018-08-07 00:11:19 +0200

    Calculate RC values

--- src/SubKeyGen.hs
+++ src/SubKeyGen.hs
@@ -10,6 +10,11 @@ import Globals (Key, numRounds, aesPolynomial)
 type KeyWord = [Word8]
 type RC = [Word8]
 
+rcVals :: [Word8]
+rcVals = map (fromIntegral . modF2m aesPolynomial) (take numRounds $ doubles 1)
+    where
+        doubles n = n:(doubles $ 2 * n)
+
 generateSubKeys :: Key -> [Key]
 generateSubKeys = undefined
 
