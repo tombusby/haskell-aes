57327557e68055b4d0f9f24dfc706ee0687e1e0c
Tom Busby <tom@busby.ninja>
2018-08-07 20:33:01 +0200

    Start work on main control structure and arg parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
-import SBox
+import System.Environment (getProgName, getArgs)
+import System.Directory (doesFileExist)
+
+import EncryptDecrypt (encrypt, decrypt)
+import Padding (pad, unpad)
+
+checkArgs :: [String] -> IO ()
+checkArgs [input, output, key] = do
+    inputReal <- doesFileExist input
+    let inputError = if inputReal then [] else input ++ " does not exist.\n"
+    let errors = inputError
+    putStr $ "Errors:\n\n" ++ inputError
 
 main :: IO ()
-main = return ()
+main = do
+    progName <- getProgName
+    args <- getArgs
+    if length args /= 3 then 
+        putStrLn $ "Usage: " ++ progName ++ " <INPUT FILENAME> <OUTPUT FILENAME> <KEY>"
+     else do
+        checkArgs args
+        
